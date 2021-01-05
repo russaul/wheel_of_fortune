5050003bbfaa24719c08697403c91f21389d4ee2
russaul <50722995+russaul@users.noreply.github.com>
2021-01-05 09:19:44 +0300

    Add files via upload

--- wheel.cpp
+++ wheel.cpp
@@ -9,6 +9,7 @@ using namespace std;
 class wheel
 {
     string word;
+    string tmp_word;
 
     public:
     
@@ -25,17 +26,48 @@ class wheel
         tmp.clear();
         tmp.seekg(0);
         srand(time(0));
-        int num_word = rand() % num;
-        num = 0;
-        while(num < num_word){
+        int num_word = rand() % num + 1;
+        for (num = 0; num < num_word; num++){
             buf.clear();
             getline(tmp, buf);
-            num++;
         }
         word = buf;
-        cout << word << endl;;
+        num_word = word.length();
+        for (num = 0; num < num_word; num++)
+            tmp_word += '_';
     };
 
+    int num_symb(char symb)
+    {
+        int i, len = tmp_word.length(), num = 0;
+        for (i = 0; i < len; i++){
+            if (tmp_word[i] == symb){
+                num++;
+            }
+        }
+        return num;
+    };
+
+    void check(char letter)
+    {
+        int i, len = word.length();
+        for (i = 0; i < len; i++){
+            if (word[i] == letter){
+                tmp_word[i] = word[i];
+            }
+        }
+    }
+
+    void game_circle()
+    {
+        char letter;
+        while (num_symb('_') != 0){
+            cout << endl << tmp_word << endl << "Input letter: ";
+            cin >> letter;
+            check(letter);
+        } 
+        cout << endl << endl << "Good!" << endl << "Your word was " << word << endl;
+    };
 
 };
 
@@ -43,8 +75,9 @@ class wheel
 int main()
 {
     string name;
-    cout << "Please, input your name:" << endl;
+    cout << "Please, input your name: ";
     cin >> name;
-    cout << "Hello, " << name << endl << "Let's play!" << endl;
+    cout << "Hello, " << name << "\n\nLet's play!" << endl;
     wheel game;
+    game.game_circle();
 }
